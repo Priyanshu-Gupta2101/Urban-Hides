7d8ba356eaf428c23c130fda216be3a9c201c5c5
Priyanshu Gupta <notmeduh420@gmail.com>
2023-10-16 00:32:47 +0530

    Hey there, added the api routes

--- client/src/app/hooks/axiosinstance.js
+++ client/src/app/hooks/axiosinstance.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://127.0.0.1:8080", // Set a base URL for all requests
+  baseURL: "https://urban-hides.vercel.app/", // Set a base URL for all requests
   // timeout: 5000, // Set a timeout (in milliseconds) for requests
   headers: {
     "Content-Type": "application/json",
